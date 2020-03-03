1d2b568f72265c147da9931aecc1d48e46e307b8
Ares <aresyolao@hotmail.com>
2020-03-02 22:03:29 -0600

    implementación de métodos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import { saludar } from './js/componentes';
 import './styles.css';
+import { Todo, TodoList } from './clases'
+import { crearTodoHtml } from './js/componentes';
+// import { Todo } from './clases/todo.class';
+// import { TodoList } from './clases/todo-list.class';
 
-const nombre = 'Ares';
-saludar(nombre);
+const todoList = new TodoList;
+const tarea = new Todo('Aprender javascript');
+// const tarea2 = new Todo('Aprender React');
+
+todoList.nuevoTodo(tarea);
+tarea.completado = true;
+// todoList.nuevoTodo(tarea2)
+console.log(todoList);
+
+crearTodoHtml(tarea);
